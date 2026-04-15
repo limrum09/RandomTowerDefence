72919da9d3dc6b298fdae8b407fa61b803e7e3d6
limrum09 <51752244+limrum09@users.noreply.github.com>
2026-04-15 14:48:41 +0900

    enemy spawn
    spawn

---
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -0,0 +1,121 @@
+using System.IO;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private Animator anim;
+
+    [SerializeField]
+    private string enemyUID;
+    [SerializeField]
+    private int level;
+    [SerializeField]
+    private string stringKey;
+    [SerializeField]
+    private string enemySkillUID;
+    [SerializeField]
+    private int basicHp;
+    [SerializeField]
+    private int increaseHP;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private int basicShield;
+    [SerializeField]
+    private int increaseShield;
+    [SerializeField]
+    private float rewardGold;
+    [SerializeField]
+    private string iconPath;
+
+    [SerializeField]
+    private int maxHP;
+    [SerializeField]
+    private int currentHP;
+    [SerializeField]
+    private int maxShield;
+    [SerializeField]
+    private int currentShield;
+
+    public int Level => level;
+    public string StringKey => stringKey;
+    public string EnemySkillUID => enemySkillUID;
+    public int MaxHP => maxHP;
+    public int MaxShield => maxShield;
+    public float MoveSpeed => moveSpeed;
+    public float RewardGold => rewardGold;
+
+    public void Init(string uid, int getLevel)
+    {
+        level = getLevel;
+        enemyUID = uid;
+
+        EnemyData data = Managers.EnemyData.GetEnemyData(enemyUID);
+
+        if(data == null)
+        {
+            Destroy(gameObject);
+        }
+
+        enemyUID = data.enemyUID;
+        stringKey = data.stringKey;
+        enemySkillUID = data.enemySkillUID;
+        basicHp = data.basicHp;
+        increaseHP = data.increaseHP;
+        moveSpeed = data.moveSpeed;
+        basicShield = data.basicShield;
+        increaseShield = data.increaseShield;
+        rewardGold = data.rewardGold;
+        iconPath = data.iconPath;
+
+        SetState();
+        SetAnimator(iconPath);
+    }
+
+    private void SetState()
+    {
+        maxHP = currentHP = basicHp + (increaseHP * level);
+        maxShield = currentShield = basicShield + (increaseShield * level);
+    }
+
+    private void SetAnimator(string path)
+    {
+        AnimatorDatas aniDatas = Resources.Load<AnimatorDatas>("Anis/AnimatorDatas");
+        RuntimeAnimatorController aniController = aniDatas.FindByCode(iconPath + "Ani");
+
+        anim.runtimeAnimatorController = aniController;
+    }
+
+    private void Die()
+    {
+        anim.SetBool("Die", true);
+    }
+
+    public void EnemyGeTakeDamage(int damage)
+    {
+        if(currentShield > 0)
+        {
+            currentShield -= damage;
+
+            if (currentShield > 0)
+                return;
+            else
+            {
+                currentHP += currentShield;
+                return;
+            }
+        }
+
+        currentHP -= damage;
+
+        if(currentHP <= 0)
+        {
+            Die();
+            return;
+        }
+    }
+}
